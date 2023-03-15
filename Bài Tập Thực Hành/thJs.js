10b2e5d673e76ece83facba48525180f55153ca9
hohoang2001 <hohoang1511@gmail.com>
2023-03-15 17:09:23 +0700

    Bài Tập JV

---
+++ Bài Tập Thực Hành/thJs.js
@@ -0,0 +1,23 @@
+
+/*function tinhtien(){
+ let sotien = document.getElementById("sotien").value;
+ let form = document.getElementById("form").value;
+ let to = document.getElementById("to").value;
+ let Result;
+if (form == "VND" && to == "USD"){
+   Result = "Result: " + (sotien / 23000) + "USD";
+}
+else if (form == "USD" && to == "VND") {
+   Result = "Result: " + (sotien * 23000) + "VND";
+}
+else if (form == "USD") {
+   Result = "Result: " + sotien + "$";
+}
+else {
+   Result = "Result: " + sotien + "VND";
+}
+document.getElementById("Result").innerHTML=Result;
+
+
+
+}*/
