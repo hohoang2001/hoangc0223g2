797c813d9ed487fdfe5554cc162ca6426033f3bc
hohoang2001 <hohoang1511@gmail.com>
2023-03-17 11:04:01 +0700

    bài Tập

--- Bài Tập Thực Hành/thJs.js
+++ Bài Tập Thực Hành/thJs.js
@@ -1,5 +1,5 @@
 
-/*function tinhtien(){
+function tinhtien(){
  let sotien = document.getElementById("sotien").value;
  let form = document.getElementById("form").value;
  let to = document.getElementById("to").value;
@@ -7,17 +7,17 @@
 if (form == "VND" && to == "USD"){
    Result = "Result: " + (sotien / 23000) + "USD";
 }
-else if (form == "USD" && to == "VND") {
-   Result = "Result: " + (sotien * 23000) + "VND";
-}
-else if (form == "USD") {
-   Result = "Result: " + sotien + "$";
-}
-else {
-   Result = "Result: " + sotien + "VND";
-}
+    else if (form == "USD" && to == "VND") {
+            Result = "Result: " + (sotien * 23000) + "VND";
+   }
+       else if (form == "USD") {
+               Result = "Result: " + sotien + "$";
+      }
+          else {
+                Result = "Result: " + sotien + "VND";
+         }
 document.getElementById("Result").innerHTML=Result;
 
 
 
-}*/
+}
